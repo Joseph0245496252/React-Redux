e168ae01e9388d297c351884becf755b7cbedd68
Joseph Asante <freecoding1@gmail.com>
2020-01-28 12:04:47 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import { activateAwesomeness, closeAwesomeness } from './redux'
+
+function App(props) {
+  return (
+    <div className="App">
+      <h1>{props.awesomeness.title || "Hello World!"}</h1>
+
+      {props.awesomeness.title ? (
+        <button> onClick={props.closeAwesomeness}close awesomeness</button>
+      ): (
+        <button
+          onClick={() => 
+            props.activateAwesomeness({ title: "I am super Awesome!!!"})
+          }
+        >
+          Activate awesomeness
+        </button>
+
+      )}
+    </div>
+  );
+}
+
+const mapStateToProps = state => ({
+  awesomeness: state.awesomeness
+});
+
+const mapDispatchToProps = {
+  activateAwesomeness,
+  closeAwesomeness  
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/redux.js
@@ -0,0 +1,41 @@
+// import React, { Component } from 'react'
+import { combineReducers, createStore } from 'redux';
+
+    // action.js
+
+export const activateAwesomeness = awesomeness => ({
+    type: 'ACTIVATE AWESOMENESS',
+    payload: awesomeness
+})  // action for activating awesomess text
+
+export const closeAwesomeness = () => ({
+    type: 'CLOSE AWESOMENESS'
+}) // activating for deactivating awesomeness text
+
+
+// reducer.js
+
+export const awesomeness = (state= {}, action) => {
+    switch (action.type) {
+        case 'ACTIVATE_AWESOMENESS':
+            return action.awesomeness;
+        case 'CLOSE_AWSOMENESS':
+            return {};
+        default:
+            return state;
+    }
+};
+
+// this is actually not required for a single reducer 
+export const reducers = combineReducers ({ awesomeness, });
+
+// sotore.js
+
+export function configureStore(initialState = {}) {
+    const store = createStore(reducers, initialState);
+    return store;
+}
+
+export const store = configureStore();
+
+
